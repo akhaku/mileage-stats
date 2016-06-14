5f834bcd6188e40f1e4b7c5cd0f8216fc0f2c667
Ammar Khaku <ammar.khaku@gmail.com>
2016-06-13 19:33:49 -0700

    Add react-charjs and draw simple line graph

--- src/shared/components/Home.js
+++ src/shared/components/Home.js
@@ -1,6 +1,8 @@
 /* eslint-env browser,node */
 import React from 'react';
 
+const timeFormat = 'MM/DD/YYYY';
+
 export default class Home extends React.Component {
   static propTypes = {
     data: React.PropTypes.arrayOf(React.PropTypes.shape({
@@ -11,17 +13,61 @@ export default class Home extends React.Component {
 
   componentDidMount() {
     require('./Home.less');
+    this.ChartJs = require('react-chartjs');
+    this.forceUpdate();
   }
 
   _renderDates(data) {
     return data.map(d => <p key={d.date.getTime()}>{d.date + ' ' + d.mileage}</p>);
   }
 
+  _formatData(data) {
+    return data.map(d => ({x: d.date, y: d.mileage}));
+  }
+
+  _getData(data) {
+    const labels = data.map(d => d.date);
+    return {
+      labels: labels,
+      datasets: [{
+        label: 'Overall miles traveled',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: this._formatData(data),
+      }],
+    };
+  }
+
+  _getOptions() {
+    return {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'miles traveled',
+      },
+      scales: {
+        xAxes: [{
+          type: 'time',
+          time: {format: timeFormat, tooltipFormat: 'll'},
+          scaleLabel: {display: true, labelString: 'Date'},
+        }],
+        yAxes: [{
+          scaleLabel: {display: true, labelString: 'miles'},
+        }],
+      },
+    };
+  }
+
   render() {
-    return (
+    const ChartJs = this.ChartJs;
+    return (typeof ChartJs === 'undefined') ? <div/> : (
       <div className="Component-Home">
         {'Hello World'}
-        {this._renderDates(this.props.data)}
+        <ChartJs.Line data={this._getData(this.props.data)} options={this._getOptions()}/>
       </div>
     );
   }
